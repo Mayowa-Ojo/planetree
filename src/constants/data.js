00c70196aa52d86063d8e62669b06650729e7b70
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-10-03 23:25:32 -0600

    feat: add mock data

---
+++ src/constants/data.js
@@ -0,0 +1,205 @@
+export const boards = {
+   "backlog": [
+      {
+         id: "065933347-3",
+         title: "Auditing information architecture",
+         description: "Listing out all of the findings from current or existing IA",
+         category: "research",
+         participants: 7,
+         tasks: {
+            total: 20,
+            completed: 5
+         },
+         attachments: 4,
+         comments: 23,
+         theme: "bg-pink-400",
+         due: ""
+      },
+      {
+         id: "462570752-8",
+         title: "Listing deliverables checklist",
+         description: "",
+         category: "planning",
+         participants: 1,
+         tasks: {
+            total: 0,
+            completed: 0
+         },
+         attachments: 0,
+         comments: 10,
+         theme: "bg-green-400",
+         due: ""
+      },
+      {
+         id: "397586987-9",
+         title: "High fidelity UI - Desktop",
+         description: "",
+         category: "Design",
+         participants: 3,
+         tasks: {
+            total: 0,
+            completed: 0
+         },
+         attachments: 6,
+         comments: 0,
+         theme: "bg-blue-400",
+         due: "Feb 26"
+      },
+      {
+         id: "646309021-2",
+         title: "Update requirements list",
+         description: "",
+         category: "Planning",
+         participants: 4,
+         tasks: {
+            total: 0,
+            completed: 0
+         },
+         attachments: 0,
+         comments: 5,
+         theme: "bg-green-400",
+         due: "Feb 24"
+      },
+   ],
+   "to do": [
+      {
+         id: "796310177-X",
+         title: "Low fidelity (wireframe) design - Mobile",
+         description: "",
+         category: "Design",
+         participants: 2,
+         tasks: {
+            total: 0,
+            completed: 0
+         },
+         attachments: 3,
+         comments: 0,
+         theme: "bg-blue-400",
+         due: "Feb 20"
+      },
+      {
+         id: "868784704-1",
+         title: "Update support documentation",
+         description: "Enhance a better guidance about support documentation for scalability purposes",
+         category: "Content",
+         participants: 1,
+         tasks: {
+            total: 26,
+            completed: 0
+         },
+         attachments: 0,
+         comments: 9,
+         theme: "bg-yellow-400",
+         due: "Feb 25"
+      },
+   ],
+   "in progress": [
+      {
+         id: "804726630-6",
+         title: "Qualitative research planning",
+         description: "",
+         category: "Research",
+         participants: 1,
+         tasks: {
+            total: 7,
+            completed: 2
+         },
+         attachments: 0,
+         comments: 3,
+         theme: "bg-pink-400",
+         due: "Feb 09"
+      },
+      {
+         id: "856728813-4",
+         title: "Brainstorm on the new looks and overall branding",
+         description: "Ideating the foundation looks",
+         category: "Planning",
+         participants: 7,
+         tasks: {
+            total: 0,
+            completed: 0
+         },
+         attachments: 3,
+         comments: 9,
+         theme: "bg-green-400",
+         due: "Today"
+      },
+      {
+         id: "501344335-0",
+         title: "Create styleguide foundation",
+         description: "",
+         category: "Design",
+         participants: 2,
+         tasks: {
+            total: 12,
+            completed: 5
+         },
+         attachments: 0,
+         comments: 18,
+         theme: "bg-blue-400",
+         due: "Feb 09"
+      },
+      {
+         id: "160517156-5",
+         title: "Improve dark mode UI across app",
+         description: "",
+         category: "Design",
+         participants: 5,
+         tasks: {
+            total: 2,
+            completed: 2
+         },
+         attachments: 5,
+         comments: 3,
+         theme: "bg-blue-400",
+         due: "Tomorrow"
+      },
+   ],
+   "needs review": [
+      {
+         id: "971060731-6",
+         title: "Environment setup and tech stack choosing",
+         description: "",
+         category: "Dev",
+         participants: 2,
+         tasks: {
+            total: 20,
+            completed: 4
+         },
+         attachments: 2,
+         comments: 15,
+         theme: "bg-red-400",
+         due: "Feb 14"
+      },
+      {
+         id: "264079108-7",
+         title: "UI improvements",
+         description: "Hero section to be improved based on the new feedback",
+         category: "Design",
+         participants: 8,
+         tasks: {
+            total: 0,
+            completed: 0
+         },
+         attachments: 1,
+         comments: 5,
+         theme: "bg-blue-400",
+         due: "Mar 03"
+      },
+      {
+         id: "166719224-8",
+         title: "Test UX",
+         description: "User experience needs to be tested across all devices - UX",
+         category: "Design",
+         participants: 3,
+         tasks: {
+            total: 6,
+            completed: 5
+         },
+         attachments: 4,
+         comments: 10,
+         theme: "bg-blue-400",
+         due: "Mar 08"
+      },
+   ]
+}
